126fd6e14e6bafe7f60e57ea98d96edce5d9b000
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-02-18 13:34:24 -0300

    Adds IContainer and ISlot Extensions

---
+++ src/TheChest.Tests.Common/Extensions/IContainerExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+using TheChest.Core.Containers.Interfaces;
+using TheChest.Core.Slots.Interfaces;
+
+namespace TheChest.Tests.Common.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for accessing internal fields of <see cref="IContainer{T}"/> instances.
+    /// </summary>
+    public static class IContainerExtensions
+    {
+        /// <summary>
+        /// Retrieves the value of the non-public or public instance field named "slots" from the specified container.
+        /// </summary>
+        /// <typeparam name="T">The type of elements contained in the <paramref name="container"/>.</typeparam>
+        /// <param name="container">The container object from which to retrieve the "slots" field value.</param>
+        /// <returns>The value of the "slots" field from the specified container.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the "slots" field does not exist on the container's type.</exception>
+        internal static object GetSlotsField<T>(this IContainer<T> container)
+        {
+            var type = container.GetType();
+
+            var field = type.GetField(
+                "slots",
+                BindingFlags.Instance | BindingFlags.NonPublic | 
+                BindingFlags.Public | BindingFlags.FlattenHierarchy
+            ) ?? throw new InvalidOperationException("Field 'slots' not found.");
+
+            return field.GetValue(container);
+        }
+        /// <summary>
+        /// Gets the array of slots contained in the specified container.
+        /// </summary>
+        /// <typeparam name="T">The type of items stored in the slots.</typeparam>
+        /// <param name="container">The container from which to retrieve the slots. </param>
+        /// <returns>An array of <see cref="ISlot{T}"/> representing the slots in the container, or <see langword="null"/> if the container does not contain slot data in the expected format.</returns>
+        public static ISlot<T>[] GetSlots<T>(this IContainer<T> container) => container.GetSlotsField() as ISlot<T>[];
+        /// <summary>
+        /// Gets the slot at the specified index from the container's array of slots.
+        /// </summary>
+        /// <typeparam name="T">The type of items stored in the slots.</typeparam>
+        /// <param name="container">The container from which to retrieve the item.</param>
+        /// <param name="index">Index of the slot to retrieve.</param>
+        /// <returns>An <see cref="ISlot{T}"/> representing the slot at the specified index in the container, or <see langword="null"/> if the container does not contain slot data in the expected format or if the index is out of bounds.</returns>
+        public static ISlot<T> GetSlot<T>(this IContainer<T> container, int index) => container.GetSlots()[index];
+        /// <summary>
+        /// Gets the content of the slot at the specified index from the container's array of slots.
+        /// </summary>
+        /// <typeparam name="T">The type of items stored in the slots.</typeparam>
+        /// <param name="container">The container from which to retrieve the item.</param>
+        /// <param name="index">Index of the slot to retrieve.</param>
+        /// <returns>An item of type <typeparamref name="T"/> representing the content of the slot at the specified index in the container, or <see langword="null"/> if the container does not contain slot data in the expected format, if the index is out of bounds, or if the slot at the specified index is empty.</returns>
+        public static T GetItem<T>(this IContainer<T> container, int index) => container.GetSlots()[index].GetContent();
+    }
+}
---
+++ src/TheChest.Tests.Common/Extensions/ISlotExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+using TheChest.Core.Slots.Interfaces;
+
+namespace TheChest.Tests.Common.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for the <see cref="ISlot{T}"/> interface.
+    /// </summary>
+    public static class ISlotExtensions
+    {
+        internal static FieldInfo GetContentField<T>(this ISlot<T> slot)
+        {
+            var type = slot.GetType();
+            var field = type.GetField(
+                "content",
+                BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy
+            ) ??
+            throw new InvalidOperationException("Field 'content' not found.");
+
+            return field;
+        }
+
+        /// <summary>
+        /// Retrieves the value stored in the specified slot, cast to the given type.
+        /// </summary>
+        /// <typeparam name="T">The type to which the slot's value will be cast and returned.</typeparam>
+        /// <param name="slot">The slot from which to retrieve the value. </param>
+        /// <returns>The value contained in the slot, cast to type <typeparamref name="T"/>.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the slot's underlying field type is not assignable to <typeparamref name="T"/>.</exception>
+        public static T GetContent<T>(this ISlot<T> slot)
+        {
+            var field = slot.GetContentField();
+
+            var fieldType = field.FieldType;
+            if (fieldType != typeof(T) && !typeof(T).IsAssignableFrom(fieldType))
+                throw new InvalidOperationException($"Field type '{fieldType}' is not assignable to '{typeof(T)}'.");
+
+            return (T)field.GetValue(slot);
+        }
+
+        /// <summary>
+        /// Retrieves a copy of the contents stored in the specified stack slot.
+        /// </summary>
+        /// <typeparam name="T">The type of elements contained in the stack slot.</typeparam>
+        /// <param name="slot">The stack slot from which to retrieve the contents.</param>
+        /// <returns>A new array containing the elements stored in the stack slot, or <see langword="null"/> if the slot is empty.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the underlying field type of the stack slot is not assignable to an array of type <typeparamref name="T"/>.</exception>
+        public static T[] GetContents<T>(this IStackSlot<T> slot)
+        {
+            var field = slot.GetContentField();
+
+            var fieldType = field.FieldType;
+            if (fieldType != typeof(T[]) && !typeof(T[]).IsAssignableFrom(fieldType))
+                throw new InvalidOperationException($"Field type '{fieldType}' is not assignable to '{typeof(T[])}'.");
+
+            var original = (T[])field.GetValue(slot);
+            if (original is null)
+                return null;
+
+            return (T[])original.Clone();
+        }
+    }
+}
