a34ca00abca0d51677b7effe156ea8f8517160b6
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-02-18 10:30:58 -0300

    Adds ReflectionExceptionHandleAttribute

---
+++ src/TheChest.Tests.Common/Attributes/ReflectionExceptionHandleAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace TheChest.Tests.Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public sealed class ReflectionExceptionHandleAttribute : Attribute { }
+}
---
+++ src/TheChest.Tests.Common/Attributes/ReflectionExceptionHandleProxy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+
+namespace TheChest.Tests.Common.Attributes
+{
+    public sealed class ReflectionExceptionHandleProxy<T> : DispatchProxy
+    {
+        private T target;
+
+        public ReflectionExceptionHandleProxy() { }
+
+        public ReflectionExceptionHandleProxy(T target)
+        {
+            this.target = Create(target);
+        }
+
+        public static T Create(T target)
+        {
+            object proxy = Create<T, ReflectionExceptionHandleProxy<T>>() 
+                ?? throw new InvalidOperationException("Failed to create proxy instance.");
+            
+            ((ReflectionExceptionHandleProxy<T>)proxy).target = target;
+
+            return (T)proxy;
+        }
+
+        protected override object? Invoke(MethodInfo? targetMethod, object?[]? args)
+        {
+            if (targetMethod == null)
+                throw new ArgumentNullException(nameof(targetMethod));
+            try
+            {
+                return targetMethod.Invoke(target, args);
+            }
+            catch (TargetInvocationException ex) when (ex.InnerException != null)
+            {
+                var inner = ex.InnerException;
+                var attribute = targetMethod.GetCustomAttribute<ReflectionExceptionHandleAttribute>();
+                
+                if (inner.GetType() != typeof(TargetInvocationException))
+                    throw inner;
+
+                if (attribute != null && inner.InnerException != null)
+                    throw inner.InnerException;
+
+                throw inner;
+            }
+        }
+    }
+}
