e3c8ca8f09ffe0a2bbcf0dfcae9a5222092ef191
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-02-18 13:04:47 -0300

    Adds ItemFactory<T>

---
+++ src/TheChest.Tests.Common/Items/Interfaces/IItemFactory.cs
@@ -0,0 +1,33 @@
+﻿namespace TheChest.Tests.Common.Items.Interfaces
+{
+    /// <summary>
+    /// Generic item creation.
+    /// There is nothing special about this interface. 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IItemFactory<T>
+    {
+        /// <summary>
+        /// Creates an item
+        /// </summary>
+        /// <returns>Any item instance</returns>
+        T CreateDefault();
+        /// <summary>
+        /// Creates an item with every property set to a random value
+        /// </summary>
+        /// <returns>Any item instance</returns>
+        T CreateRandom();
+        /// <summary>
+        /// Creates an array of items using <see cref="CreateDefault"/>
+        /// </summary>
+        /// <param name="amount">Size of the returned array of items</param>
+        /// <returns>An array with size <paramref name="amount"/></returns>
+        T[] CreateMany(int amount);
+        /// <summary>
+        /// Creates an array of items using <see cref="CreateRandom"/>
+        /// </summary>
+        /// <param name="amount">Size of the returned array of items</param>
+        /// <returns>An array with size <paramref name="amount"/></returns>
+        T[] CreateManyRandom(int amount);
+    }
+}
---
+++ src/TheChest.Tests.Common/Items/ItemFactory.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using TheChest.Tests.Common.Items.Interfaces;
+using TheChest.Tests.Common.Extensions;
+
+namespace TheChest.Tests.Common.Items
+{
+    public sealed class ItemFactory<T> : IItemFactory<T>
+    {
+        public T CreateDefault()
+        {
+            var type = typeof(T);
+            var instance = Activator.CreateInstance(type) ??
+                throw new InvalidOperationException($"Could not create instance of type {type.FullName}");
+
+            return (T)instance;
+        }
+
+        public T CreateRandom()
+        {
+            var type = typeof(T);
+            var instance = Activator.CreateInstance(type) ??
+                throw new InvalidOperationException($"Could not create instance of type {type.FullName}");
+
+            var instanceType = instance.GetType();
+            if(instanceType.IsEnum || instanceType.IsPrimitive)
+            {
+                if (instanceType.IsEnum)
+                {
+                    var values = ((T[])instanceType.GetEnumValues()).Skip(1).ToArray();
+                    values.Shuffle();
+                    return (T)values.GetValue(0)!;
+                }
+                return SetRandomValue<T>(instanceType);
+            }
+
+            var fields = instanceType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var field in fields)
+            {
+                field.SetValue(instance, ItemFactory<T>.SetRandomValue<dynamic>(field.FieldType));
+            }
+            return (T)instance;
+        }
+
+        public T[] CreateMany(int amount)
+        {
+            return Enumerable.Repeat(CreateDefault(), amount).ToArray();
+        }
+
+        public T[] CreateManyRandom(int amount)
+        {
+            return Enumerable.Repeat(CreateRandom(), amount).ToArray();
+        }
+
+        private static Y SetRandomValue<Y>(Type type)
+        {
+            var random = new Random();
+
+            return type switch
+            {
+                var t when t == typeof(int) || t == typeof(short) || t == typeof(long)
+                    => (Y)(object)random.Next(1, 1000),
+
+                var t when t == typeof(double) || t == typeof(float) || t == typeof(decimal)
+                    => (Y)(object)(random.NextDouble() * 1000),
+
+                var t when t == typeof(byte)
+                    => (Y)(object)(byte)random.Next(1, 255),
+
+                var t when t == typeof(string)
+                    => (Y)(object)Guid.NewGuid().ToString(),
+
+                var t when t == typeof(bool)
+                    => (Y)(object)(random.Next(0, 2) == 1),
+
+                _ => throw new NotImplementedException(
+                    $"Random generation for type {typeof(Y).Name} is not implemented.")
+            };
+        }
+
+    }
+}
