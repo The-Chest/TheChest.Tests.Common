98a7d5deb73eafa959d3a2a9ac73408e725368e8
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-02-18 12:46:48 -0300

    Adds ArrayExtensions

---
+++ src/TheChest.Tests.Common/Extensions/ArrayExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TheChest.Tests.Common.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for working with arrays.
+    /// </summary>
+    public static class ArrayExtensions
+    {
+        /// <summary>
+        /// Randomly reorders the elements of the specified array in place.
+        /// </summary>
+        /// <param name="items">The array whose elements will be shuffled.</param>
+        public static void Shuffle(this Array items)
+        {
+            var rng = new Random();
+            int n = items.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                var item = items.GetValue(n);
+                var item2 = items.GetValue(k);
+
+                items.SetValue(item, k);
+                items.SetValue(item2, n);
+            }
+        }
+    }
+}
