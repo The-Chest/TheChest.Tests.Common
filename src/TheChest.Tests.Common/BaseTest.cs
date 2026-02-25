568dba530e765056efe9bf56e941e438f604e05b
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-02-25 09:05:07 -0300

    Adds BaseTest class

---
+++ src/TheChest.Tests.Common/BaseTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TheChest.Tests.Common.DependencyInjection;
+using TheChest.Tests.Common.Items;
+using TheChest.Tests.Common.Items.Interfaces;
+
+namespace TheChest.Tests.Common
+{
+    public abstract class BaseTest<T>
+    {
+        protected readonly DIContainer configurations;
+        protected readonly Random random;
+
+        protected BaseTest(Action<DIContainer> configure)
+        {
+            this.configurations = new DIContainer();
+
+            configure(this.configurations);
+
+            if (!this.configurations.IsRegistered<IItemFactory<T>>())
+            {
+                this.configurations.Register<IItemFactory<T>, ItemFactory<T>>();
+            }
+
+            this.random = new Random();
+        }
+    }
+}
