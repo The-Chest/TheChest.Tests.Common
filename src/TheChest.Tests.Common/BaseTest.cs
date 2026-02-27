f315dfffd12a90e7fe29012213d085da84316afd
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-02-27 16:04:05 -0300

    Adds XML Docs to BaseTest<T>

--- src/TheChest.Tests.Common/BaseTest.cs
+++ src/TheChest.Tests.Common/BaseTest.cs
@@ -5,11 +5,28 @@ using TheChest.Tests.Common.Items.Interfaces;
 
 namespace TheChest.Tests.Common
 {
+    /// <summary>
+    /// Base class for tests that provides a dependency injection container and a <see cref="Random"/> instance. 
+    /// </summary>
+    /// <typeparam name="T">The item type used by the test factory.</typeparam>
     public abstract class BaseTest<T>
     {
+        /// <summary>
+        /// Container for dependency registrations used by the test.
+        /// </summary>
         protected readonly DIContainer configurations;
+        /// <summary>
+        /// Random instance for generating test values.
+        /// </summary>
         protected readonly Random random;
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="BaseTest{T}"/>.
+        /// </summary>
+        /// <remarks>
+        /// Executes the provided <paramref name="configure"/> action to register services in the container, and registers a default <see cref="IItemFactory{T}"/> when none is already registered.
+        /// </remarks>
+        /// <param name="configure">Action to configure the test <see cref="DIContainer"/>.</param>
         protected BaseTest(Action<DIContainer> configure)
         {
             this.configurations = new DIContainer();
