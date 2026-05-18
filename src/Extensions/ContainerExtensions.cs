e7e6db71231a525e3d1f3d53b6f022e94cf90539
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-05-18 14:34:26 -0300

    Adds Extension methods to get private fields

---
+++ src/Extensions/ContainerExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+namespace TheChest.Tests.Common.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for working with container types, including checking interface implementations and accessing specific fields.
+    /// </summary>
+    public static class ContainerExtensions
+    {
+        /// <summary>
+        /// Determines the container type that implements the specified interface.
+        /// </summary>
+        /// <param name="containerType">The type to check for implementation of the specified interface.</param>
+        /// <param name="interfaceType">The interface type to verify against the container type.</param>
+        /// <returns>The <paramref name="containerType"/> if it implements the specified <paramref name="interfaceType"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="interfaceType"/> is not an interface or if <paramref name="containerType"/> does not implement <paramref name="interfaceType"/>.</exception>
+        public static Type GetTypeIfImplements(this Type containerType, Type interfaceType)
+        {
+            if (!interfaceType.IsInterface)
+                throw new ArgumentException($"'{interfaceType.FullName}' is not an interface.");
+
+            if (!interfaceType.IsAssignableFrom(containerType))
+                throw new ArgumentException($"Type '{containerType.FullName}' does not implement '{interfaceType.FullName}'.");
+
+            return containerType;
+        }
+
+        /// <summary>
+        /// Retrieves the value of the non-public or public instance field named "slots" from the specified container.
+        /// </summary>
+        /// <param name="container">The container object from which to retrieve the "slots" field value.</param>
+        /// <returns>The value of the "slots" field from the specified container.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the "slots" field does not exist on the container's type.</exception>
+        public static object GetSlotsField(this object container)
+        {
+            var type = container.GetType();
+
+            var field = type.GetField(
+                "slots",
+                BindingFlags.Instance | BindingFlags.NonPublic |
+                BindingFlags.Public | BindingFlags.FlattenHierarchy
+            ) ?? throw new InvalidOperationException("Field 'slots' not found.");
+
+            return field.GetValue(container);
+        }
+    }
+}
---
+++ src/Extensions/SlotExtensions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace TheChest.Tests.Common.Extensions
+{
+    public static class SlotExtensions
+    {
+        public static Type GetSlotTypeByConstructor<TSlotInterface>(this Type containerType, string slotParameterName = "slots")
+        {
+            var constructor = containerType.GetConstructors()
+                    .FirstOrDefault(ctor =>
+                    {
+                        var parameters = ctor.GetParameters();
+                        var slotParamType = parameters.Length > 0 ? parameters[0].ParameterType : null;
+                        if (slotParamType is null)
+                            return false;
+                        return
+                            parameters.Length == 1 &&
+                            slotParamType.IsArray &&
+                            typeof(TSlotInterface).IsAssignableFrom(slotParamType.GetElementType());
+                    })
+                    ?? throw new ArgumentException($"Container type '{containerType.FullName}' does not have a suitable constructor.");
+
+            var slotParameter = constructor.GetParameters().FirstOrDefault(x => x.Name == slotParameterName)
+                ?? throw new ArgumentException($"Container type '{containerType.FullName}' does not have a constructor with {typeof(TSlotInterface).Name}[].");
+
+            var slotType = slotParameter.ParameterType.GetElementType();
+            if (!typeof(TSlotInterface).IsAssignableFrom(slotType))
+                throw new ArgumentException($"Type '{slotType?.FullName}' does not implement {typeof(TSlotInterface).FullName}.");
+
+            return slotType!;
+        }
+
+        public static FieldInfo GetContentField(this object slot)
+        {
+            var type = slot.GetType();
+
+            while (type != null)
+            {
+                var field = type.GetField(
+                    "content",
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public
+                );
+
+                if (field != null)
+                    return field;
+
+                type = type.BaseType;
+            }
+
+            throw new InvalidOperationException("Field 'content' not found.");
+        }
+
+        public static T GetContentFieldValue<T>(this object slot)
+        {
+            var field = slot.GetContentField();
+
+            var value = field.GetValue(slot);
+            if (value is null)
+                return default!;
+
+            return (T)value;
+        }
+
+        public static T[] GetContentFieldValues<T>(this object slot)
+        {
+            var field = slot.GetContentField();
+
+            var original = field.GetValue(slot);
+            if (original is null)
+                return Array.Empty<T>();
+
+            var array = original as object[];
+            if (array is null)
+                return Array.Empty<T>();
+
+            return array.Where(x => !(x is null)).Cast<T>().ToArray();
+        }
+    }
+}
